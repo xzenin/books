eb52ddbdb07643e222a16f4b07f3db11547ab374
xzenin <xzen.in@hotmail.com>
2026-04-03 03:09:24 +0530

    Batch testing

--- test.bat
+++ test.bat
@@ -1,1 +1,4 @@
-./write.bat sita "What is the story of Sita in Ramayana, and how does it reflect the themes of love, sacrifice, and devotion in the epic?"
+echo off
+echo Running book writing : command usage: write.bat [book_name] [chapter_count] [gist]
+rem./write.bat sita 4 "What is the story of Sita in Ramayana, and how does it reflect the themes of love, sacrifice, and devotion in the epic?"
+./write.bat %1 2 %2 
